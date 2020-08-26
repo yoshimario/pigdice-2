e51c5585919357d8b34b055d26f72b251cdc3fd1
Michael Watts <wattsjmichael@gmail.com>
2020-08-26 13:54:14 -0700

    adds dice roll and players
    Co-Authored by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // Player
 
-function Player(name) {
-    this.name = name;
-    this.pointTotal = 0;
-    this.turnPointTotal = 0;
-    // isTurn;
-    // id
+function Player(name,id,isTurn) {
+    this.name = name,
+    this.id = id,
+    this.isTurn = isTurn,
+    this.pointTotal = 0,
+    this.turnPointTotal = 0
 }
 
 
@@ -18,23 +18,55 @@ var dice = {
     }
 }
 
+Player.prototype.roll = function() {
+    const roll = dice.roll();
+    if (roll === 1) {
+        this.turnPointTotal = 0;
+        this.isTurn = false;
+    } else {
+        this.turnPointTotal += roll;
+        if ((pointTotal + turnPointTotal) >= 100) {
+            // trigger you win function
+        } 
+    }
+}
+
+Player.prototype.hold = function() {
+    this.pointTotal += this.turnPointTotal;
+    this.turnPointTotal = 0;
+    this.isTurn = false;
+}
 
 
+function p1PointTotal () {
+  let pointTotal = (randoNumber + turnPointTotal) ;
+  if (pointTotal >= 100) {
+    alert("You Win!")
+  } else if {dice.roll()
+    
+  }
+}
 
 
 // UI Logic
 $(document).ready(function(){
-    $("#formOne").submit(function(event){
+   // Player Sign Up Logic
+   let playerOne;
+   let playerTwo;
+    $("#playerOneSignUp").submit(function(event) {
         event.preventDefault();
-        var result = dice.roll();
-        console.log(result);
-        
-
-        /*
-        let button = $(".button");
-        button.onclick = function () {
-
-            console.log(result);
-        } */
+        const name = $("input#playerOneName").val();
+        playerOne = new Player(name,1,true);
+        console.log(playerOne);
+        $("#playerOneSignUpGroup").fadeOut(1000,function() {
+            $("#playerOneGameGroup").fadeIn(1000);
+        });
     })
-    });
+    $("#playerTwoSignUp").submit(function(event) {
+        event.preventDefault();
+        const name = $("input#playerTwoName").val();
+        playerTwo = new Player(name,2,false);
+        console.log(playerTwo);
+        //$("#playerTwoSignUpGroup").fadeOut(1000);
+    })
+});
